3dbd2cbbefa4ba9cd2157cbbbc2d86d41a074dfb
René de Montigny <e2395944@cmaisonneuve.qc.ca>
2024-03-05 23:08:32 -0500

    Régler bug affichage RHI dans recette/show.php

--- views/recette/show.php
+++ views/recette/show.php
@@ -10,23 +10,31 @@
     </div>
 
     <div class="liste_ingredient--container">
-     {% if recettehasingredients is defined %}
-     {% for recettehasingredient in recettehasingredients %}
-        {% for ingredient in ingredients %}
-        {% for umesure in umesures %}
-        {% if umesure.id == recettehasingredient.unite_mesure_id %}
-            {% if ingredient.id == recettehasingredient.ingredient_id %}
-            {{recettehasingredient.recette_id}}
-                <ul class="liste_ingredient">
-                    <li><a href="{{ base }}/recettehasingredient/edit?recette_id={{recettehasingredient.recette_id}}"><span class="liste_ingredient__qte"></span>{{ recettehasingredient.quantite }}&nbsp;<span class="liste_ingredient__umesure"></span>{{ umesure.nom }}&nbsp;<span class="liste_ingredient__ingredient">{{ ingredient.nom }}&nbsp;</span></a></li>
-                </ul>
-            {% endif %}
-            {% endif %}
-        {% endfor %}
-        {% endfor %}
-     {% endfor %}
-     {% endif %}
- </div>
+        {% if recettehasingredients is defined %}{% for recettehasingredient in recettehasingredients %}
+
+            
+                {% for ingredient in ingredients %}{% if ingredient.id == recettehasingredient.ingredient_id and recette.id == recettehasingredient.recette_id %}     
+                    
+                        
+                            
+                            {{recettehasingredient.recette_id}}
+                                <ul class="liste_ingredient">
+                                    <div class="btn-delete_rhi">
+                                        <li><a href="{{ base }}/recettehasingredient/edit?recette_id={{recettehasingredient.recette_id}}&ingredient_id={{ recettehasingredient.ingredient_id }}"><span class="liste_ingredient__qte"></span>{{ recettehasingredient.quantite }}&nbsp;<span class="liste_ingredient__umesure"></span>{% for umesure in umesures %}{% if umesure.id == recettehasingredient.unite_mesure_id %}{{ umesure.nom }}{% endif %}{% endfor %}&nbsp;<span class="liste_ingredient__ingredient">{{ ingredient.nom }}&nbsp;</span></a></li>
+                                        <form action="{{base}}/recettehasingredient/delete" method="post">
+                                            <input type="hidden" name="recette_id" value="{{ recette.id }}">
+                                            <button class="btn block red">Delete</button>
+                                        </form>
+                                    </div>
+                                </ul>
+                            
+                        
+                    
+                {% endif %}{% endfor %}
+            
+
+        {% endfor %}{% endif %}
+    </div>
 
 </div>
 <div class="recette__infos">
